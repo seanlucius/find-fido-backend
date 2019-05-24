d96bfa34ea0e1e451ad3776b2c77ea0866488ceb
seanlucius <seanlucius22@icloud.com>
2019-05-24 11:08:18 -0400

    stuff

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,26 @@
 class SightingsController < ApplicationController
+  def index
+    render json: Sighting.all
+  end
+
+  def show
+    render json: Sighting.find(params[:id])
+  end
+
+  def create
+    render json: Sighting.create(sighting_params)
+  end
+
+  def update
+    Sighting.find(params[:id]).update(sighting_params)
+    render json: Sighting.find(params[:id])
+  end
+
+  def destroy
+    render json: Sighting.find(params[:id]).destroy()
+  end
+
+  def sighting_params
+    params.require(:sighting).permit(:user_id, :picture, :latitude, :longitude, :species, :name, :breed, :sex, :age, :color, :defining_features, :description)
+  end
 end
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,5 +1,5 @@
 class Sighting < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :possible_matches
   has_many :lost_pets, through: :possible_matches
 end
